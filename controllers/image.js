a99072eeb9c851a23f76aedb7c9197535ded31e7
Elena <tereshka07@gmail.com>
2018-05-23 18:23:05 +0900

    remove

--- controllers/image.js
+++ controllers/image.js
@@ -5,10 +5,6 @@ const app = new Clarifai.App({
   });
 
 const handleApiCall = () => (req, res) => {
-	console.log("--------------------");
-	console.log(req.body);
-	console.log(req.body.input);
-
 	app.models.predict("a403429f2ddf4b49b307e318f00e528b", req.body.input)
 	.then(data => {
 		res.json(data);
