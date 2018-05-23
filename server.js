12fe0094a773a33b8f52bfd37493b68c43233bd0
Elena <tereshka07@gmail.com>
2018-05-23 10:33:41 +0900

    adding database

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT;
 const db = knex({
 	client: 'pg',
 	connection: {
-		host: process.env.DATABASE_URL,
+		connectionString: process.env.DATABASE_URL,
 		ssl: true
 	}
 });
